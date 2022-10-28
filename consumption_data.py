c50adcae84ead2299b01e8e6738d969cf88644bf
Adam D Walker <Adam.Walker1@nottingham.ac.uk>
2022-10-28 17:05:48 +0100

    add the skeleton for the consumption data file manager

--- consumption_data.py
+++ consumption_data.py
@@ -41,3 +41,34 @@ class ConsumptionData(ABC):
             bool: True only if data exists for the whole range, else False.
         """
         pass
+
+
+class ConsumptionDataFile(ConsumptionData):
+    """Access energy consumption data from a file.
+
+    Consumption data file must be in a CSV file format with three columns and
+    must include headers. The columns must contain:
+
+        1. Consumption data in kWh.
+        2. Start time in ISO 8601 format.
+        3. End time in ISO 8601 format.
+
+    Example of a valid consumption data file:
+
+        Consumption (kWh), Start, End
+        0.507, 2021-09-29T00:00:00+01:00, 2021-09-29T00:30:00+01:00
+    """
+
+    def __init__(self, filename: str, headers: list[str]):
+        """Initialise the consumption data from file.
+
+        Args:
+            filename (str): Name of the CSV file to load data from.
+        """
+        pass
+
+    def get_usage_for_range(self, start: int, end: int) -> int:
+        return 0
+
+    def has_usage_for_range(self, start: int, end: int) -> bool:
+        return False
